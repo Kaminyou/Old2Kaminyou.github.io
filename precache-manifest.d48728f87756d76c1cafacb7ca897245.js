7317f37d213dae945ae6de8a1b7cbed4cd277398
Kaminyou <ikaminyou@gmail.com>
2021-02-05 00:03:05 +0800

    Updates

---
+++ precache-manifest.d48728f87756d76c1cafacb7ca897245.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37b6f07db48c14e227babf5cc8199ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f92d5e0bafe59dbd4c32",
+    "url": "/static/css/main.a720a78b.chunk.css"
+  },
+  {
+    "revision": "61b094443537951d879f",
+    "url": "/static/js/2.14cb94a5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.14cb94a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92d5e0bafe59dbd4c32",
+    "url": "/static/js/main.48923736.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8f9cdb30045308bb049bb295162c02b6",
+    "url": "/static/media/IMG_9374.8f9cdb30.jpg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "b782bc5cfc3ffac035987e1a808394a3",
+    "url": "/static/media/resume.b782bc5c.pdf"
+  }
+]);
